f9f44383f96a27cc51a808da387eeeb8abafc9ba
ximartin <xiaomar0916@gmail.com>
2026-03-16 23:59:38 +0800

    Add files via upload

---
+++ 147.cpp
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode(0)   
+        curr = head             
+        while curr:
+            prev = dummy       
+            while prev.next and prev.next.val < curr.val:
+                prev = prev.next
+            
+            next_node = curr.next  
+            curr.next = prev.next
+            prev.next = curr
+            
+            curr = next_node     
+        
+        return dummy.next
+        
